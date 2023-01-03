980633b0f6b78e934130711a020c04c5b1065a88
zjy <jiangyuan.zhou@palmpay-inc.com>
2023-01-03 19:27:08 +0800

    no message

--- ZCSwiftKit/ZCSwiftKit/ZCKit/BasicMacro.swift
+++ ZCSwiftKit/ZCSwiftKit/ZCKit/BasicMacro.swift
@@ -8,22 +8,22 @@
 import UIKit
 import Foundation
 
+func Adapt(_ radix: CGFloat) -> CGFloat { ((radix) * kRadio375) }
 func Image(_ name: String) -> UIImage { UIImage(named: name) ?? UIImage()}
 
 func ArrNonnil(_ arr: Any?) -> [Any?] { Array.valid(arr) }
 func StrNonnil(_ str: Any?) -> String { String.valid(str) }
 func DicNonnil(_ dic: Any?) -> [AnyHashable : Any?] { Dictionary.valid(dic) }
 
-let ZSWid: CGFloat = min(UIScreen.main.bounds.size.width, UIScreen.main.bounds.size.height)
-let ZSHei: CGFloat = max(UIScreen.main.bounds.size.width, UIScreen.main.bounds.size.height)
-let ZSRadio375: CGFloat = (ZSWid / 375.0)
-let ZSScreen: CGRect = UIScreen.main.bounds
-let ZSPixel: CGFloat = (1.0 / UIScreen.main.scale)
-let IsIPhoneX: Bool = { ZSHei / ZSWid > 2.0 || ZSHei / ZSWid < 0.5 }()
-func ZSA(_ radix: CGFloat) -> CGFloat { ((radix) * ZSRadio375) }
+let kWid: CGFloat = min(UIScreen.main.bounds.size.width, UIScreen.main.bounds.size.height)
+let kHei: CGFloat = max(UIScreen.main.bounds.size.width, UIScreen.main.bounds.size.height)
+let kRadio375: CGFloat = (kWid / 375.0)
+let kScreen: CGRect = UIScreen.main.bounds
+let kPixel: CGFloat = (1.0 / UIScreen.main.scale)
+let kIsIPhoneX: Bool = { kHei / kWid > 2.0 || kHei / kWid < 0.5 }()
 
-let ZSNaviHei: CGFloat = (IsIPhoneX ? 88.0 : 64.0)
-let ZSNaviBarHei: CGFloat = (IsIPhoneX ? 44.0 : 44.0)
-let ZSStuBarHei: CGFloat = (IsIPhoneX ? 44.0 : 20.0)
-let ZSTabHei: CGFloat = (IsIPhoneX ? 83.0 : 49.0)
-let ZSBomSafeHei: CGFloat = (IsIPhoneX ? 34.0 : 20.0)
+let kNaviHei: CGFloat = (kIsIPhoneX ? 88.0 : 64.0)
+let kNaviBarHei: CGFloat = (kIsIPhoneX ? 44.0 : 44.0)
+let kStuBarHei: CGFloat = (kIsIPhoneX ? 44.0 : 20.0)
+let kTabHei: CGFloat = (kIsIPhoneX ? 83.0 : 49.0)
+let kBomSafeHei: CGFloat = (kIsIPhoneX ? 34.0 : 20.0)
